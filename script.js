897056853488ba6a60154d457853700750b315ab
vitornoms1 <vitornoms1@gmail.com>
2025-05-13 12:01:09 -0300

    blur e menu arrumados

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const menuToggle = document.getElementById('menu-toggle');
 const sideMenu = document.getElementById('side-menu');
 const overlay = document.getElementById('overlay');
+const body = document.body;  // Adicionei esta linha
 
 menuToggle.addEventListener('click', () => {
   sideMenu.classList.toggle('active');
   overlay.classList.toggle('active');
   menuToggle.classList.toggle('active'); // <- aqui ativa o X
+  
+  // Adiciona ou remove a classe no-scroll no body
+  body.classList.toggle('no-scroll');
 });
 
 overlay.addEventListener('click', () => {
   sideMenu.classList.remove('active');
   overlay.classList.remove('active');
   menuToggle.classList.remove('active'); // <- aqui remove o X
+  
+  // Remove a classe no-scroll do body
+  body.classList.remove('no-scroll');
 });
